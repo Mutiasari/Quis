c4ad570d659f4fa87925867a3858ea1bac21cdb4
Mutia Sari <Sarimutia26@yahoo.co.id>
2015-10-20 23:39:09 +0700

    versi 1.0.0

---
+++ src/perhitungan/Persegi.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perhitungan;
+
+/**
+ *
+ * @author Mahasiswa
+ */
+public class Persegi {
+  int luas;
+  public int persegi(int sisi){
+      luas = sisi * sisi * sisi;
+      return luas;
+  }
+  }
+
---
+++ src/perhitungan/Segitiga.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perhitungan;
+
+/**
+ *
+ * @author Mahasiswa
+ */
+public class Segitiga {
+   double luas;
+   public double segitiga(double alas, double tinggi){
+       luas = alas * tinggi / 2;
+       return luas;
+   }
+
+    
+   
+}
